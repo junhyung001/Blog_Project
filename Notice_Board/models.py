69a486221d29f49fb9713864a4ef6db57d1add7f
junhyung001 <junhyung8325@gmail.com>
2023-07-20 23:56:32 +0900

    modles.py, views.py 삭제 수정기능 추가중

--- Notice_Board/models.py
+++ Notice_Board/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Borad(models.Model):
+    todo = models.CharField(max_length=20)
+    description = models.TextField()
+    important = models.BooleanField(default=False)
+    complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'[{self.pk}] {self.borad}'
+
+    def get_absolute_url(self):
+        return '/borad/'
--- Notice_Board/views.py
+++ Notice_Board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 def Board(request):
@@ -6,3 +6,15 @@ def Board(request):
         request,
         'Notice_Board/board.html',
         )
+
+def DeleteTodo(request, pk):
+    delete_todo = get_object_or_404(Board, pk=pk)
+    delete_todo.delete()
+    return redirect('/todo/')
+
+def CompletedTodo(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    todo.complete = True
+    todo.save()
+    return redirect('/todo/')
+
