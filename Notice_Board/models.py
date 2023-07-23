dfe05ab0819ef7672dae8c4c059275d31cdc073c
junhyung001 <junhyung8325@gmail.com>
2023-07-23 22:13:11 +0900

    블로그 글쓰기 기능 구현중 테스팅 실패

--- Notice_Board/models.py
+++ Notice_Board/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
-# Create your models here.
 class Board_list(models.Model):
-    todo = models.CharField(max_length=20)
+    board = models.CharField(max_length=20)
     description = models.TextField()
     important = models.BooleanField(default=False)
     complete = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'[{self.pk}] {self.borad2}'
+        return f'[{self.pk}] {self.board}'
 
-    # def get_absolute_url(self):
-    #     return '/borad/'
+    def get_absolute_url(self):
+        return '/board/'
--- Notice_Board/views.py
+++ Notice_Board/views.py
@@ -1,13 +1,43 @@
-from django.shortcuts import get_object_or_404, render
 from Notice_Board.models import Board_list
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+def DeleteBoard(request, pk):
+    delete_todo = get_object_or_404(Board_list, pk=pk)
+    delete_todo.delete()
+    return redirect('/board/')
+
+def CompletedBoard(request, pk):
+    todo = get_object_or_404(Board_list, pk=pk)
+    todo.complete = True
+    todo.save()
+    return redirect('/board/')
+
+class BoardUpdate(UpdateView):
+    model = Board_list
+    fields = ['todo', 'description', 'important']
+
+    template_name = 'Notice_Board/board_update_form.html'
+
+
+class BoardCreate(LoginRequiredMixin, CreateView):
+    model = Board_list
+    fields = ['todo', 'description', 'important']
+    login_url = '/accounts/sigin/'
+
+def Todos(request):
+    boards = Board.objects.all().order_by('pk')
 
 # Create your views here.
 def Board(request):
+    boards = Board_list.objects.all().order_by('pk')
     return render(
         request,
         'Notice_Board/board.html',
+        {'board':boards},
         )
-
 
 def index(request):
     board_list = Board_list.objects.all()
@@ -18,20 +48,3 @@ def index(request):
             'board_list' : board_list,
         }
     )
-
-
-
-
-
-
-def DeleteTodo(request, pk):
-    delete_todo = get_object_or_404(Board, pk=pk)
-    delete_todo.delete()
-    return redirect('/todo/')
-
-def CompletedTodo(request, pk):
-    todo = get_object_or_404(Todo, pk=pk)
-    todo.complete = True
-    todo.save()
-    return redirect('/todo/')
-
